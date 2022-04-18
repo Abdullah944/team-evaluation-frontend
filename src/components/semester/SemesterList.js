4119b5a8f81b65373a9babbfb97168d05fccfbb0
Abdullah90 <darkside944@gmail.com>
2022-04-18 20:15:33 +0300

    make semesters item list & show the list in the list page

--- src/components/semester/SemesterList.js
+++ src/components/semester/SemesterList.js
@@ -1,16 +1,26 @@
+import { observer } from "mobx-react";
 import React from "react";
 import semesterStore from "../../stores/semesterStore";
 import SemesterItem from "./SemesterItem";
 
 //? MAPPING THROUGH THE ITEMS & SHOW THEM:
 function SemesterList() {
-  const semesterList = () => {
-    semesterStore.semester.map((semester) => (
-      <SemesterItem key={semester.id} semester={semester} />
-    ));
-  };
+  const semesterList =
+    semesterStore.semester && semesterStore.semester.length !== 0 ? (
+      semesterStore.semester
+        .map((semester) => (
+          <SemesterItem semester={semester} key={semester.id} />
+        ))
+        .reverse()
+    ) : (
+      <p> No semesters yet</p>
+    );
 
-  return <div>{semesterList}</div>;
+  return (
+    <div>
+      <h3>{semesterList}</h3>
+    </div>
+  );
 }
 
-export default SemesterList;
+export default observer(SemesterList);
